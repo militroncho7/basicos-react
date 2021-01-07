01b7a3222da9d2faf47a4371abede907573358e9
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-01-07 19:07:10 +0100

    finnish proyect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Producto from './components/Producto';
+import Carrito from './components/Carrito';
 
 function App() {
+  
+  //Crear listado de productos
+  const [productos, guardarProductos] = useState([
+    {id: 1, nombre: "camisa ReatJS", precio: 50},
+    {id: 2, nombre: "camisa VueJS", precio: 40},
+    {id: 3, nombre: "camisa Node.JS", precio: 30},
+    {id: 4, nombre: "camisa Angular", precio: 20},
+  ]);
 
-//Obtener la fecha (en este caso en el footer)
-const fecha = new Date().getFullYear();
+  //State para un carrito de la compra
+  const [carrito, agregarProducto] = useState([]);
 
-return (
-  <Fragment>
-    <Header 
-      titulo='Tienda Virtual'
-    />
+  //Obtener la fecha (en este caso en el footer)
+  const fecha = new Date().getFullYear();
 
-    <Footer 
-      fecha={fecha}
-    />
-  </Fragment>
-  );
+  return (
+    <Fragment>
+      <Header 
+        titulo='Tienda Virtual'
+      />
+
+      <h1>Lista de Procutos</h1>
+      {productos.map(producto => (
+          <Producto 
+            key={producto.id}
+            producto={producto}
+            productos={productos}
+            carrito={carrito}
+            agregarProducto={agregarProducto}
+          />
+      ))}
+
+      <Carrito 
+        carrito={carrito}
+        agregarProducto={agregarProducto}
+      />
+
+      <Footer 
+        fecha={fecha}
+      />
+    </Fragment>
+    );
 };
 
 export default App;
