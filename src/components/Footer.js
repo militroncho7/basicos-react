9d2ca10659d1e28cdd0d7c71c899918240b24823
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-01-07 11:16:53 +0100

    start basic react

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+//imr
+import React from 'react';
+
+const Footer = ({fecha}) => (
+        <footer>
+            <p>Todos los derechos reservados &copy; {fecha}</p>
+        </footer>
+    );
+
+export default Footer;
+
+//sfc
+// const  = () => {
+//     return (  );
+// }
+
+// export default ;
